e4e4a2e4d543a72ca8c1bf67c9632a6c23a7ba08
Ander0907 <zapataander18@gmail.com>
2023-02-20 07:46:27 -0500

    segundo commit

---
+++ ejercicio_2.py
@@ -0,0 +1,29 @@
+"""
+name = "Anderson Zapata Bedoya"
+print("hello", name)
+
+#Ejemplo casting (convertir una variable de un tipo de dato a otro)
+variable= 5
+variable_1 = float (variable)
+variable_2 = str(variable)
+
+#sumar_datos = variable + variable_2
+
+print(variable,variable_1,variable_2)
+print(type(variable_2))
+print(type(variable_1))
+print(type(variable))
+"""
+
+#tipos de datos
+variable1 = "Hola" #string (texto)
+variable2 = 20     #int (entero)
+variable3 = 20.8   #float (decimal)
+variable4 = 1j     #complex
+variable5 = True   #bool (logico)
+variable6 = ["cantar","jugar","estudiar"]#list
+variable7 = ("cantar","jugar","estudiar")#tupla
+carro= {"color":"rojo","velocidad":300,"modelo":"GTR35"}#dict
+#print(variable5)
+#print(variable6)
+#print(carro)
