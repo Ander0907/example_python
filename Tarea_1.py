05bb054bc03a33150a4b68bd697aba0a287939f3
Ander0907 <zapataander18@gmail.com>
2023-02-20 11:44:34 -0500

    Actividad

---
+++ Tarea_1.py
@@ -0,0 +1,73 @@
+"""
+Cajero = "Hola, ingrese su tarjeta"
+Saldo =10000
+variable = "Su saldo es 10000"
+Cantidad = 5000
+resta = Saldo - Cantidad
+variable2 = "Tu saldo ahora es 5000"
+print(resta)
+print(Cajero)
+print(variable)
+print(Saldo)
+print(resta)
+print(variable2)
+#Variables: cajero, saldo, cantidad y resta.
+"""
+
+
+"""
+Contacto_de_amigo = 3002457742
+Estado_de_conexion = "En linea"
+Mensaje = "Oe, todo bien"
+print(Contacto_de_amigo)
+print(Estado_de_conexion)
+print(Mensaje)
+#Variables: contacto de amigo, estado de conexion y mensaje.
+"""
+
+"""
+Total_a_pagar = 90000
+Medio_de_pago = "Tarjeta de credito"
+Usuarios = "Anderson Zapata"
+Num_tarjeta = 1020112035
+print(Total_a_pagar)
+print(Medio_de_pago)
+print(Usuarios)
+print(Num_tarjeta)
+
+#Variables: Total a pagar, medio de pago(tarjeta de crédito), usuario, numero de la tarjeta.
+"""
+
+
+"""
+variable= "Tipo de lavado: Manual"
+variable2= "Cantidad de ropa: 5"
+varaible3= "Tipo de ropa: Delicada"
+variable4 = "Producto a utilizar: Detergente liquido"
+variable5= "Hora de inicio 18:00 pm"
+variable6= "Hora de terminar 17:30 pm"
+
+print(variable)
+print(variable2)
+print(varaible3)
+print(variable4)
+print(variable5)
+print(variable6)
+#Variables: tipo de lavado, cantidad de ropa, tipo de ropa, productos a utilizar.
+
+"""
+
+
+
+"""
+Dispositivo_movil = "Samsung J2 core"
+Saldo = 1000
+Numero_de_telefono = 3007374682
+Disponibilidad_amigo = "Ocupado"
+print(Dispositivo_movil)
+print(Saldo)
+print(Numero_de_telefono)
+print(Disponibilidad_amigo)
+#Variables: dispositivo movil,número de teléfono y disponibilidad del amigo.
+
+"""
