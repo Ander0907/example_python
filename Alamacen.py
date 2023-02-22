5be93f4a59f18e56f2613a792a3b4788c59669b2
Ander0907 <zapataander18@gmail.com>
2023-02-22 10:00:47 -0500

    evidencia

--- Alamacen.py
+++ Alamacen.py
@@ -1,36 +1,28 @@
 #Productos
-nombres = {
+precio_productos = {
     "Zapatos" : 350000,
     "Tenis" : 280000,
     "Camisetas" : 175000,
     "Jeans" : 200000
 }
-print(nombres)
+print(precio_productos)
 
 #Total
-price = 1005000
+price = precio_productos["Camisetas"] + precio_productos["Jeans"] + precio_productos["Tenis"] + precio_productos["Zapatos"]
 print("Total:", price)
 
 #Promedio de venta
-num1 = 350000
-num2 = 280000
-num3 = 175000
-num4 = 200000
-sumar = num1 + num2 + num3 + num4
-cantidad = 4
-promedio = sumar / cantidad
-print(f"El promedio es:", promedio)
+promedio = price / 4
+print("El promedio es:", promedio)
 
 #Subir el precio de los Jeans en un 6.2%
 porcentaje_aumento = 6.2
-valor_original = 200000
-aumento = valor_original * (porcentaje_aumento / 100)
-valor_con_aumento = valor_original + aumento
-print(f"El valor con aumento es: {valor_con_aumento}")
+aumento = precio_productos["Jeans"] * (porcentaje_aumento / 100)
+valor_con_aumento = precio_productos["Jeans"] + aumento
+print("El valor con aumento es:", valor_con_aumento)
 
 #Disminuir el precio de los Zapatos en un 0.8%
-porcentaje_aumento2 = 0.8
-valor_original2 = 350000
-descuento= valor_original2 * (porcentaje_aumento2/100)
-valor_con_descuento= valor_original2 - porcentaje_aumento2
-print(f"El valor con disminucion es: {valor_con_descuento}")
+porcentaje_disminucion = 0.8
+disminucion = precio_productos["Zapatos"] * (porcentaje_disminucion/100)
+precio_total=  precio_productos["Zapatos"] - disminucion
+print("El valor con disminucion es:", precio_total)
